1ce79d1f5f6938c05f171b5691fd7658132ca616
archilleu <313720180@qq.com>
2017-11-28 15:32:17 +0800

    fix single_template filter

--- functions.php
+++ functions.php
@@ -634,21 +634,22 @@ add_filter('single_template', 'my_single_template');
 * Single template function which will choose our template
 */
 function my_single_template($single) {
-global $wp_query, $post;
+	global $wp_query, $post;
 
-/**
-* Checks for single template by category
-* Check by category slug and ID
-*/
-foreach((array)get_the_category() as $cat) :
+	/**
+	* Checks for single template by category
+	* Check by category slug and ID
+	*/
+	foreach((array)get_the_category() as $cat) :
 
-if(file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
+		if(file_exists(SINGLE_PATH . '/single-cat-' . $cat->slug . '.php'))
+			return SINGLE_PATH . '/single-cat-' . $cat->slug . '.php';
 
-elseif(file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
-return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
+		elseif(file_exists(SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php'))
+			return SINGLE_PATH . '/single-cat-' . $cat->term_id . '.php';
 
-endforeach;
+		return $single;
+	endforeach;
 }
 /*
 *  End define a constant path to our single template folder
